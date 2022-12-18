ccd1b78dde978ed97fb133088e4d9a427bb3b977
Divya <beingdivyadixit@gmail.com>
2022-12-18 16:25:56 +0530

    Updated To Calculate The Lenght Of Line Program

--- ConsoleApp_Line Comparison ProblemDay3/Program.cs
+++ ConsoleApp_Line Comparison ProblemDay3/Program.cs
@@ -7,6 +7,16 @@ namespace ConsoleApp_Line_Comparison_ProblemDay3
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Line Comparison");
+            Console.WriteLine("Please Enter X1 & Y1 Co-Ordinates:");
+            var X1 = Convert.ToDouble(Console.ReadLine());
+            var Y1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Please Enter X2 & Y2 Co - Ordinates:");
+            var X2 = Convert.ToDouble(Console.ReadLine());
+            var Y2 = Convert.ToDouble(Console.ReadLine());
+            double X = Math.Pow((X2 - X1), 2);
+            double Y = Math.Pow((Y2 - Y1), 2);
+            double lengthOfLine = Math.Sqrt(X + Y);
+            Console.WriteLine("Length Of Line is :" + lengthOfLine);
         }
     }
 }
