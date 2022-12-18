2b2ec5635ef49aff27fce122bb28c7e09bf2a920
Divya <beingdivyadixit@gmail.com>
2022-12-18 16:44:13 +0530

    Updated For Checking the Equality Of Line

--- ConsoleApp_Line Comparison ProblemDay3/Program.cs
+++ ConsoleApp_Line Comparison ProblemDay3/Program.cs
@@ -13,10 +13,28 @@ namespace ConsoleApp_Line_Comparison_ProblemDay3
             Console.WriteLine("Please Enter X2 & Y2 Co - Ordinates:");
             var X2 = Convert.ToDouble(Console.ReadLine());
             var Y2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Please Enter X3 & Y3 Co-Ordinates:");
+            var X3 = Convert.ToDouble(Console.ReadLine());
+            var Y3 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Please Enter X4 & Y4 Co - Ordinates:");
+            var X4 = Convert.ToDouble(Console.ReadLine());
+            var Y4 = Convert.ToDouble(Console.ReadLine());
             double X = Math.Pow((X2 - X1), 2);
             double Y = Math.Pow((Y2 - Y1), 2);
-            double lengthOfLine = Math.Sqrt(X + Y);
-            Console.WriteLine("Length Of Line is :" + lengthOfLine);
+            double lengthOfFirstLine = Math.Sqrt(X + Y);
+            Console.WriteLine("Length Of First Line" + lengthOfFirstLine);
+            double x = Math.Pow((X4 - X3), 2);
+            double y = Math.Pow((Y4 - Y3), 2);
+            double lengthOfSecondLine = Math.Sqrt(x + y);
+            Console.WriteLine("Length Of Second Line" + lengthOfSecondLine);
+            if (lengthOfFirstLine == lengthOfSecondLine)
+            {
+                Console.WriteLine("Lines are Equal");
+            }
+            else
+            {
+                Console.WriteLine("Lines are not Equal");
+            }
         }
     }
 }
