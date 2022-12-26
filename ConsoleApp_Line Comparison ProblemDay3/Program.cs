e182e552066ea480f4bcc909498f2150fa2f8e08
Divya <beingdivyadixit@gmail.com>
2022-12-26 16:38:27 +0530

    updated to compare to method

--- ConsoleApp_Line Comparison ProblemDay3/Program.cs
+++ ConsoleApp_Line Comparison ProblemDay3/Program.cs
@@ -6,39 +6,43 @@ namespace ConsoleApp_Line_Comparison_ProblemDay3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Line Comparison");
-            Console.WriteLine("Please Enter X1 & Y1 Co-Ordinates:");
-            var X1 = Convert.ToDouble(Console.ReadLine());
-            var Y1 = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Please Enter X2 & Y2 Co - Ordinates:");
-            var X2 = Convert.ToDouble(Console.ReadLine());
-            var Y2 = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Please Enter X3 & Y3 Co-Ordinates:");
-            var X3 = Convert.ToDouble(Console.ReadLine());
-            var Y3 = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Please Enter X4 & Y4 Co - Ordinates:");
-            var X4 = Convert.ToDouble(Console.ReadLine());
-            var Y4 = Convert.ToDouble(Console.ReadLine());
-            double X = Math.Pow((X2 - X1), 2);
-            double Y = Math.Pow((Y2 - Y1), 2);
-            double lengthOfFirstLine = Math.Sqrt(X + Y);
-            Console.WriteLine("Length Of First Line" + lengthOfFirstLine);
-            double x = Math.Pow((X4 - X3), 2);
-            double y = Math.Pow((Y4 - Y3), 2);
-            double lengthOfSecondLine = Math.Sqrt(x + y);
-            Console.WriteLine("Length Of Second Line" + lengthOfSecondLine);
-            if (lengthOfFirstLine == lengthOfSecondLine)
-            {
-                Console.WriteLine("Lines are Equal");
-            }
-            else if (lengthOfFirstLine > lengthOfSecondLine)
-            {
-                Console.WriteLine("First Line is Greater than Second Line");
-            }
+         
+            // user input for first line
+            Console.WriteLine("Enter x1 and y2 of first line");
+            var x1 = Convert.ToDouble(Console.ReadLine());
+            var y1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter x2 and x2 of first line");
+            var x2 = Convert.ToDouble(Console.ReadLine());
+            var y2 = Convert.ToDouble(Console.ReadLine());
+            double length1= length(x1, y1, x2, y2);
+
+            // user input for second line
+            Console.WriteLine("Enter x1 and y2 of second line");
+            var x3 = Convert.ToDouble(Console.ReadLine());
+            var y3 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter x2 and x2 of second line");
+            var x4 = Convert.ToDouble(Console.ReadLine());
+            var y4 = Convert.ToDouble(Console.ReadLine());
+            double length2 =length(x3, y3, x4, y4); 
+
+            // Using Compare Method For comparing both line
+            int status = (length1.CompareTo(length2));
+            if (status > 0)
+                Console.WriteLine(" Length of first line : {0} is greater then Length of second line : {1}", length1, length2);
+            else if (status < 0)
+                Console.WriteLine(" Length of first line : {0} is less then Length of second line : {1}", length1, length2);
             else
-            {
-                Console.WriteLine("First Line is lesser than Second Line");
-            }
+                Console.WriteLine(" Length of first line : {0} is equal to Length of second line : {1}", length1, length2);
+
+        }
+          // creating  method to caculate the length
+        public static double length(double X, double Y, double X1, double Y1)
+        {
+            double L1 = Math.Pow((X - Y), 2);
+            double L2 = Math.Pow((X1 - Y1), 2);
+            double LengthOfLine = Math.Sqrt(L1 + L2);
+            return LengthOfLine;
+
         }
     }
 }
